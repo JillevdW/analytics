e3ae35857ef7ba02d49d2580f60a37061bf4a8b3
JillevdW <jillevdweerd@icloud.com>
2019-05-08 14:16:44 +0200

    updates AnalyticsController to use new model names

--- src/Http/Controllers/AnalyticsController.php
+++ src/Http/Controllers/AnalyticsController.php
@@ -5,13 +5,13 @@ namespace Jvdw\Analytics\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 
-use Jvdw\Analytics\Models\Analytics;
+use Jvdw\Analytics\Models\AppAnalyticsEvents;
 
 class AnalyticsController extends Controller
 {
     
     public function index() {
-        return Analytics::all();
+        return AppAnalyticsEvents::all();
     }
 
 }
